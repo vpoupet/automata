78d7a1121622870c5ede6e190851ebfe43f48e1a
Albin <albin.caminade@gmail.com>
2024-07-16 17:08:30 +0200

    feat : fonctionnalité d'update ok

--- src/components/EditGrid.tsx
+++ src/components/EditGrid.tsx
@@ -1,7 +1,6 @@
 import { Automaton } from "../classes/Automaton.ts";
 import { Cell } from "../classes/Cell.ts";
-import {Conjunction, Negation, simplifyDNF} from "../classes/Clause.ts";
-import { Rule } from "../classes/Rule.ts";
+import {adaptRule, ConjunctionRule, Rule, RuleOutput} from "../classes/Rule.ts";
 import RuleGrid from "../classes/RuleGrid.ts";
 import "../style/Cell.css";
 import {Coordinates, Signal} from "../types.ts";
@@ -117,135 +116,26 @@ export default function EditGrid({
         return ruleGrid;
     }
 
-    function addNegationToActiveRules(
-        newRuleBool: Rule,
-        activeRulesBool: Set<Rule>
-    ): RuleGrid[] {
-        const newRulesGrid: RuleGrid[] = [];
-        for (const rule of activeRulesBool) {
-            let newClause = new Conjunction([
-                rule.condition,
-                new Negation(newRuleBool.condition),
-            ]).toDNF();
-            newClause = simplifyDNF(newClause)
-            for (let j = 0; j < newClause.subclauses.length; j++) {
-                newRulesGrid.push(
-                    getRuleGrid(
-                        new Rule(newClause.subclauses[j],rule.outputs)
-                    )
-                );
-            }
-        }
-        console.log(
-            "la liste des nouvelles règles à rajouter ! : ",
-            newRulesGrid
-        );
-        newRulesGrid.push(getRuleGrid(newRuleBool));
-        return newRulesGrid;
-    }
-
-    function addAdaptedRules(activeRulesBool: Set<Rule>) {
-        console.log('alèdes')
-        const oldOutput = getOutputFromRules(activeRulesBool);
-        if (oldOutput.equalsOutputs(grid.outputs)) {
-            console.log("les outputs sont les mêmes, on ne fait rien");
-            return;
-        }
-
-        const newRuleBool = RuleGrid.makeRule(grid);
-
-        //pour chaque règle active, on vérifie les outputs
-        for (const rule of activeRulesBool) {
-            for (let i=0; i<rule.outputs.length;i++) {
-                const tabIndex : number[]=[];
-                newRuleBool.outputs.some((output, index) => {
-                    //si au moins 1 output est contenu dans la règle active, on le supprime de la nouvelle règle !
-                  if (output.signal===rule.outputs[i].signal && output.neighbor===rule.outputs[i].neighbor && output.futureStep===rule.outputs[i].futureStep){
-                      tabIndex.push(index);
-                      return true;
-                  }
-                    return false;
-                })
-                //si le nombre d'output à supprimer est égal au nombre d'output de la règle
-                //on la tej des règles actives en plus
-                for (const index of tabIndex) {
-                    newRuleBool.outputs.splice(index, 1);
-                    console.log("on supprime l'output : ", rule.outputs[i]);
-                }
-                if (rule.outputs.length === tabIndex.length) {
-                    console.log("on supprime la règle : ", rule);
-                    activeRulesBool.delete(rule);
-                }
-            }
-        }
-
-        //on ajoute la négation de l'input de la nouvelle règle à chaque règle active
-
-        const rulesModified = addNegationToActiveRules(newRuleBool, activeRulesBool);
-
-        for (const rule of rulesModified) {
-            //si la grille des outputs est vide on l'enlève
-            if (rule.outputs.every((row) => row.every((cell) => cell.signals.size === 0))) {
-                const index = rulesModified.indexOf(rule);
-                if (index !== -1) {
-                    rulesModified.splice(index, 1);
-                }
-            }
-        }
-
-        //On enlève du tableau de règles celles qui doivent changer
-        //on enlève dans reglesbools les règles contenus dans le tableau activeRulesBool
-        for (const rule of activeRulesBool) {
-            const index = reglesbools.indexOf(rule);
-            if (index !== -1) {
-                reglesbools.splice(index, 1);
-            }
-        }
-        const rules = [...reglesbools.map((rule) => getRuleGrid(rule))];
-        rules.push(...rulesModified);
-        setRulesGrid(rules);
-    }
-
-    function getOutputFromRules(activeRulesBool: Set<Rule>): RuleGrid {
-        const newGrille = RuleGrid.withSize(2 * radius, nbFutureSteps);
-        const conffromgrid = newGrille.getConfigurationFromGrid();
-        for (const rule of activeRulesBool) {
-            automaton.setRules([rule]);
-            automaton.updateParameters();
-            setAutomaton(automaton);
-            const conf = automaton.makeDiagram(conffromgrid, grid.outputs.length);
-            newGrille.setGridFromConfigurations(conf);
-        }
-        return newGrille;
-    }
-
     function modifyRule() {
-        const rulesToModify = new Set<Rule>();
-        const config = grid.getConfigurationFromGrid();
-
-        for (let ruleNbr = 0; ruleNbr < reglesbools.length; ruleNbr++) {
-            for (let i = 0; i < grid.inputs.length; i++) {
-                if (
-                    reglesbools[ruleNbr].condition.eval(
-                        //PAS BIEN : on prend pas en compte si une grid plus grande
-                        config.getNeighborhood(i, -2, 2)
-                    )
-                ) {
-                    rulesToModify.add(reglesbools[ruleNbr]);
-                    console.log('on ajoute la règle num : ', ruleNbr, "de valeur : ", reglesbools[ruleNbr]);
-                }
+        const ruleFromGrid =RuleGrid.makeRule(grid.clone()) ;
+        const newRules : Rule[] = [];
+        let setOutput: Set<RuleOutput>;
+        for (const regle of reglesbools){
+            const ruleAndOutput = adaptRule(regle as ConjunctionRule , ruleFromGrid);
+            setOutput = new Set([...setOutput, ...ruleAndOutput.outputs]);
+            newRules.push(...ruleAndOutput.rules);
+        }
+        console.log('les nouvelles règles : ', newRules);
+        for (let i = 0; i < ruleFromGrid.outputs.length; i++) {
+            if (setOutput.has(ruleFromGrid.outputs[i])) {
+                ruleFromGrid.outputs.splice(i, 1);
+                i--;
             }
         }
-
-        if (rulesToModify.size === 0) {
-            console.log(
-                "pas de règle à modifier, on ajoute une nouvelle règle"
-            );
-            saveGridAsRule();
-        } else {
-            console.log("ajout de règles and stuff", rulesToModify);
-            addAdaptedRules(rulesToModify);
+        if (ruleFromGrid.outputs.length > 0) {
+            newRules.push(ruleFromGrid);
         }
+        setRulesGrid(newRules.map(getRuleGrid));
     }
 
     // Make list of active and negated signals on the active cells
