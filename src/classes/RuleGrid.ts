adb14b8c4463fc82862d4076cca3681d73f39402
vpoupet <victor.poupet@gmail.com>
2024-09-13 08:45:06 +0200

    feat: RuleGrid rule condition can now be a single literal

--- src/classes/RuleGrid.ts
+++ src/classes/RuleGrid.ts
@@ -1,6 +1,6 @@
 import Cell, { InputCell } from "./Cell.ts";
-import { Conjunction, ConjunctionOfLiterals, Literal } from "./Clause.ts";
-import Rule, { ConjunctionRule, RuleOutput } from "./Rule.ts";
+import Clause, { Conjunction, Literal } from "./Clause.ts";
+import Rule, { RuleOutput } from "./Rule.ts";
 
 class RuleGrid {
     inputCells: InputCell[];
@@ -45,11 +45,17 @@ class RuleGrid {
             return false;
         }
         for (let row = 0; row < this.outputCells.length; row++) {
-            if (this.outputCells[row].length !== other.outputCells[row].length) {
+            if (
+                this.outputCells[row].length !== other.outputCells[row].length
+            ) {
                 return false;
             }
             for (let col = 0; col < this.outputCells[row].length; col++) {
-                if (!this.outputCells[row][col].equals(other.outputCells[row][col])) {
+                if (
+                    !this.outputCells[row][col].equals(
+                        other.outputCells[row][col]
+                    )
+                ) {
                     return false;
                 }
             }
@@ -58,19 +64,15 @@ class RuleGrid {
         return true;
     }
 
-    makeRule(): ConjunctionRule {
+    makeRule(centerOrigin: boolean = true): Rule {
         return new Rule(
-            this.makeRuleCondition(true),
-            this.makeRuleOutputs()
-        ) as ConjunctionRule;
+            this.makeRuleCondition(centerOrigin),
+            this.makeRuleOutputs(centerOrigin)
+        );
     }
 
-    makeRuleCondition(centerOrigin: boolean = true): ConjunctionOfLiterals {
-        let shift = 0;
-        if (centerOrigin) {
-            shift = -this.getRadius();
-        }
-
+    makeRuleCondition(centerOrigin: boolean = true): Clause {
+        const shift = centerOrigin ? -this.getRadius() : 0;
         const literals: Literal[] = [];
         this.inputCells.forEach((cell, cellIndex) => {
             cell.signals.forEach((signal) => {
@@ -82,16 +84,20 @@ class RuleGrid {
                 literals.push(literal);
             });
         });
-        return new Conjunction(literals) as ConjunctionOfLiterals;
+        if (literals.length === 1) {
+            return literals[0];
+        }
+        return new Conjunction(literals);
     }
 
-    makeRuleOutputs(): RuleOutput[] {
+    makeRuleOutputs(centerOrigin: boolean = true): RuleOutput[] {
+        const shift = centerOrigin ? -this.getRadius() : 0;
         const outputs: RuleOutput[] = [];
         this.outputCells.forEach((row, rowIndex) => {
             row.forEach((cellule, colIndex) => {
                 cellule.signals.forEach((signal) => {
                     const ruleOutput = new RuleOutput(
-                        colIndex - this.getRadius(),
+                        colIndex + shift,
                         signal,
                         rowIndex + 1
                     );
