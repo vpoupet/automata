04a5fac76ffdc34e6b64ad581a897bf21aed3d3a
Victor Poupet <victor.poupet@gmail.com>
2024-09-13 13:35:30 +0200

    github pages deployment setup

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: "", // Set base to an empty string for relative paths
+    base: "/", // Set base to an empty string for relative paths
 });
